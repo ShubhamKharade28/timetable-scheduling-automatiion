fcb6cb01bbadf008ff0c45b896ac4296d153f91e
Shubham Kharade <shubham.kharade28@gmail.com>
2025-04-06 20:51:02 +0530

    crossover function modified

--- test/distribution/crossover.py
+++ test/distribution/crossover.py
@@ -1,19 +1,25 @@
 import random
 
-def crossover(parent1, parent2):
-    # Randomly select a crossover point
-    crossover_point = random.randint(1, len(parent1) - 1)
-    
+def crossover(parent1, parent2):    
     # Create two new offspring by swapping parts of the parents
     offspring1 = {faculty_id: [] for faculty_id in parent1}
     offspring2 = {faculty_id: [] for faculty_id in parent2}
-    
+
     for faculty_id in parent1:
-        if random.random() > 0.5:
-            offspring1[faculty_id] = parent1[faculty_id][:crossover_point] + parent2[faculty_id][crossover_point:]
-            offspring2[faculty_id] = parent2[faculty_id][:crossover_point] + parent1[faculty_id][crossover_point:]
+        parent1_courses = parent1[faculty_id]
+        parent2_courses = parent2[faculty_id]
+
+        # Choose crossover point safely (between 1 and min length - 1)
+        min_len = min(len(parent1_courses), len(parent2_courses))
+        if min_len > 1 and random.random() > 0.5:
+            crossover_point = random.randint(1, min_len-1)
+
+            offspring1[faculty_id] = parent1_courses[:crossover_point] + parent2_courses[crossover_point:]
+            offspring2[faculty_id] = parent2_courses[:crossover_point] + parent1_courses[crossover_point:]
         else:
-            offspring1[faculty_id] = parent1[faculty_id]
-            offspring2[faculty_id] = parent2[faculty_id]
+            # If too short to crossover or skipped by chance
+            offspring1[faculty_id] = parent1_courses[:]
+            offspring2[faculty_id] = parent2_courses[:]
+
     
     return offspring1, offspring2
